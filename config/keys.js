49ca0c573e2f1dd79ad6649f58c7f0c733e329ce
saud9030 <saud.amakn@gmail.com>
2019-04-12 08:10:16 +0300

    satup client side (reactstrap, concurrently)

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb://saud00:saud1234@mernshoppinglist-shard-00-00-rgjld.mongodb.net:27017,mernshoppinglist-shard-00-01-rgjld.mongodb.net:27017,mernshoppinglist-shard-00-02-rgjld.mongodb.net:27017/test?ssl=true&replicaSet=mernShoppingList-shard-0&authSource=admin&retryWrites=true"
+    "mongodb://saud2030:saud2030@mernshoppinglist-shard-00-00-rgjld.mongodb.net:27017,mernshoppinglist-shard-00-01-rgjld.mongodb.net:27017,mernshoppinglist-shard-00-02-rgjld.mongodb.net:27017/test?ssl=true&replicaSet=mernShoppingList-shard-0&authSource=admin&retryWrites=true"
 };
