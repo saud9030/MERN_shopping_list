2b85c8d6fdf047d8b68fbaade4e3c34fb192ccd4
saud9030 <saud.amakn@gmail.com>
2019-04-12 09:25:52 +0300

    (Client-side) show items, ability to add and remove an item with some CSS affect.

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import uuid from "uuid";
+
+class ShoppingList extends Component {
+  state = {
+    items: [
+      { id: uuid(), name: "Eggs" },
+      { id: uuid(), name: "Milk" },
+      { id: uuid(), name: "Coffee" },
+      { id: uuid(), name: "Tea" }
+    ]
+  };
+
+  render() {
+    // this will pull out items from the state, so don't need to use this.state.items
+    const { items } = this.state;
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const name = prompt("added item");
+            if (name) {
+              this.setState(state => ({
+                items: [...state.items, { id: uuid(), name: name }]
+              }));
+            }
+          }}
+        >
+          Add Item
+        </Button>
+        <ListGroup>
+          <TransitionGroup className="shopping-list">
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={() => {
+                      this.setState(state => ({
+                        items: state.items.filter(item => item.id !== id)
+                      }));
+                    }}
+                  >
+                    &times;
+                  </Button>
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+export default ShoppingList;
